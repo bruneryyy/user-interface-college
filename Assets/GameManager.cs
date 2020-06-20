c811c0d3570a64f8ffca1cdfd1c061b422ac45a7
Bruno <buhlovatotomlinson@gmail.com>
2020-06-19 22:15:22 -0300

    Jogo Pong criado

---
+++ Assets/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Ball")]
+    public GameObject ball;
+
+    [Header("Player 1")]
+    public GameObject player1Paddle;
+    public GameObject player1Goal;
+
+    [Header("Player2")]
+    public GameObject player2Paddle;
+    public GameObject player2Goal;
+
+    [Header("ScoreUI")]
+    public GameObject Player1Text;
+    public GameObject Player2Text;
+
+    private int Player1Score;
+    private int Player2Score;
+
+    public void Player1Scored()
+    {
+        Player1Score++;
+        Player1Text.GetComponent<TextMesh>().text = Player1Score.ToString();
+        
+    }
+    public void Player2Scored()
+    {
+        Player2Score++;
+        Player2Text.GetComponent<TextMesh>().text = Player2Score.ToString();
+        
+    }
+    private void ResetPosition()
+    {
+        ball.GetComponent<Ball>().Reset();
+        player1Paddle.GetComponent<Paddle>().Reset();
+        player2Paddle.GetComponent<Paddle>().Reset();
+    }
+}
